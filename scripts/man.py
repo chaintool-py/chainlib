393bbd2fc298a7b509b34ee486a328ab99b9d2f5
lash <dev@holbrook.no>
2022-02-21 23:43:57 +0000

    WIP Add last arg to man maps and specs, arg flags settable through cli

--- scripts/man.py
+++ scripts/man.py
@@ -1,6 +1,8 @@
+import sys
+from hexathon import strip_0x
 from chainlib.cli.man import DocGenerator
 
-
-g = DocGenerator(0xffffffff)
+b = bytes.fromhex(strip_0x(sys.argv[1]))
+g = DocGenerator(int.from_bytes(b, byteorder='little'))
 g.process()
 print(g)
